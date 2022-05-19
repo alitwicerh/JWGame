d0e39586578f16375608453bc5faeb4bfcccfb7a
alitwicerh <55243860+alitwicerh@users.noreply.github.com>
2022-05-19 14:18:22 +0200

    2

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
-const TOTAL_STARS = 12;
-
-let game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game');
+let game = new Phaser.Game(600, 800, Phaser.CANVAS, 'game');
+let palabras;
+let active;
 
 let mainState = {
     preload: loadAssets,
@@ -11,19 +11,19 @@ let mainState = {
 let player; 
 
 game.state.add("main", mainState);
-
-
-
-
 game.state.start('main');
 
 function loadAssets(){
-    game.load.image();
+    game.load.image('fondo','/assets/imgs/Space.png');
+    game.load.image('player','/assets/imgs/player.png');
 }
 
 function initialiseGame(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
+    game.add.image(0,0,'fondo');
+    let player = game.add.image(260,730,'player').scale.setTo(0.04,0.04);
+    palabras = ['trial','bad', 'had','hard','joke','eat','my','pussy'];
+    
 
     cursors = game.input.keyboard.createCursorKeys();
 
