0160915ff170bf97edb1747e733f60dd91e49e61
alitwicerh <55243860+alitwicerh@users.noreply.github.com>
2022-05-19 21:01:48 +0200

    game.js movement

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ let palabras;
 let active;
 let enemies;
 let spawn;
-
-
+let urga;
+let player;
 
 let mainState = {
     preload: loadAssets,
@@ -13,15 +13,15 @@ let mainState = {
     update: gameUpdate
 };
 
-let player;
+
 
 game.state.add("main", mainState);
 game.state.start('main');
 
 function loadAssets(){
-    game.load.image('fondo','/assets/imgs/Space.png');
-    game.load.image('player','/assets/imgs/player.png');
-    game.load.spritesheet('owp','/assets/imgs/asteroide.png');
+    game.load.image('fondo','assets/imgs/Space.png');
+    game.load.image('player','assets/imgs/player.png');
+    game.load.spritesheet('owp','assets/imgs/asteroide.png');
 }
 
 function initialiseGame(){
@@ -30,7 +30,7 @@ function initialiseGame(){
     game.world.setBounds(0,0,600,800);
     game.add.image(0,0,'fondo');
 
-    let player = game.add.image(260,730,'player').scale.setTo(0.04,0.04);
+    player = game.add.image(260,730,'player').scale.setTo(0.04,0.04);
     game.physics.arcade.enable(player);
 
     enemies = game.add.group();
@@ -38,13 +38,18 @@ function initialiseGame(){
     game.physics.arcade.enable(enemies);
 
     function spawn(image){
-     let urga = enemies.create(280,20,image).scale.setTo(0.05,0.05);
-     //urga.body.bounce.x = urga.body.bounce.y = 1;
-     //urga.body.setBounds = Phaser.CANVAS.x, Phaser.CANVAS.y;
-     urga.enableBody = true;
-     //urga.body.
-     //urga.body.gravity.y = 200;
+        urga = enemies.create(Phaser.Math.between(0,600),10,image);
+        urga.scale.setTo(0.05,0.05)
 
+        urga.body.bounce.x = urga.body.bounce.y = 1;
+        urga.body.setBounds = Phaser.CANVAS.x, Phaser.CANVAS.y;
+        urga.enableBody = true;
+        let angle = Phaser.Math.between(-10,10);
+        let algo = Math.tan(angle)
+
+        let po = Phaser.Math.distance(urga.x,urga.y, player.x, player.y);
+        game.physics.arcade.moveToXY(urga, 270 + (algo*po), 730, 30);
+        
     }
     spawn('owp');
 
@@ -53,6 +58,14 @@ function initialiseGame(){
 
 }
 
+function enemyFollows() {
+    //for(let i = 0; i < 9 ; i++){
+        game.physics.arcade.moveToXY(urga, Phaser.Math.rotateToAngle(Phaser.Math.between(-10,10)), 730, 30);
+   //}
+    
+}
+
 function gameUpdate(){
-    //spawn('owp');
+   
+
 }
