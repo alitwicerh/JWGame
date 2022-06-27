8f587fb4a953ae6d07766113a5912d9070f9e853
alitwicerh <55243860+alitwicerh@users.noreply.github.com>
2022-06-27 18:20:53 +0200

    movimiento + letra

--- js/game.js
+++ js/game.js
@@ -6,6 +6,8 @@ let enemies;
 let spawn;
 let urga;
 let player;
+let urgapalabra;
+var style = { font: "bold 32px Arial", fill: "#fff"};
 
 let mainState = {
     preload: loadAssets,
@@ -26,30 +28,41 @@ function loadAssets(){
 
 function initialiseGame(){
 
+    
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.world.setBounds(0,0,600,800);
     game.add.image(0,0,'fondo');
 
-    player = game.add.image(260,730,'player').scale.setTo(0.04,0.04);
+    player = game.add.image(260,730,'player');
+    player.scale.setTo(0.04,0.04);
     game.physics.arcade.enable(player);
 
     enemies = game.add.group();
     enemies.enableBody = true;
     game.physics.arcade.enable(enemies);
 
+    palabras = game.add.group();
+    palabras.enableBody = true;
+    game.physics.arcade.enable(palabras);
+
     function spawn(image){
         urga = enemies.create(Phaser.Math.between(0,600),10,image);
-        urga.scale.setTo(0.05,0.05)
+        urga.scale.setTo(0.05,0.05);
+        urgapalabra = game.add.text(urga.x,urga.y + urga.width,'Drag me around', style ,palabras);
+        //urgapalabra.position = urga.position;
 
         urga.body.bounce.x = urga.body.bounce.y = 1;
         urga.body.setBounds = Phaser.CANVAS.x, Phaser.CANVAS.y;
         urga.enableBody = true;
         let angle = Phaser.Math.between(-10,10);
-        let algo = Math.tan(angle)
+        let tangente = Math.tan(angle*Math.PI/180);
 
-        let po = Phaser.Math.distance(urga.x,urga.y, player.x, player.y);
-        game.physics.arcade.moveToXY(urga, 270 + (algo*po), 730, 30);
-        
+        let distancia = Phaser.Math.distance(urga.x,urga.y, player.x, player.y);
+        console.log("playerpos " ,player.x, player.y);
+        console.log("urgapos " ,urga.x,urga.y);
+        game.physics.arcade.moveToXY(urga, 270 + (tangente*distancia), 800, 30);
+        console.log("tangente*distancia= " ,tangente*distancia , tangente, distancia);
     }
     spawn('owp');
 
@@ -60,12 +73,14 @@ function initialiseGame(){
 
 function enemyFollows() {
     //for(let i = 0; i < 9 ; i++){
-        game.physics.arcade.moveToXY(urga, Phaser.Math.rotateToAngle(Phaser.Math.between(-10,10)), 730, 30);
+    game.physics.arcade.moveToXY(urga, Phaser.Math.rotateToAngle(Phaser.Math.between(-10,10)), 730, 30);
    //}
     
 }
 
 function gameUpdate(){
-   
+    //for(urgapalabra in urgapalabra){
+        game.physics.arcade.moveToXY(urgapalabra, urga.x, urga.y + urga.width, 30);
+   // }
 
 }
